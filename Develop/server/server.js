4f9c9fbfb6b927f61a181c86d18a86552ef81b2c
Daniel Polk <Danielpolk7@icloud.com>
2023-11-25 13:28:32 -0500

    implemented Apollo server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,27 +2,101 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-// const { ApolloServer } = require('@apollo/server');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// const apolloServer = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
+const typeDefs = `#graphql
+
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+
+  type Book {
+    title: String
+    author: String
+  }
+
+
+  # The "Query" type is special: it lists all of the available queries that
+
+  # clients can execute, along with the return type for each. In this
+
+  # case, the "books" query returns an array of zero or more Books (defined above).
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'Dreams in the Witch House',
+    author: 'H.P. Lovecraft'
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+
+
+const startApolloServer = async () => {
+  await apolloServer.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(apolloServer));
+
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
